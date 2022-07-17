d05630b73f37d8a0d445bce010505d10dfecd84e
abdalrzag eisa <bmjfdrh@gmail.com>
2022-07-17 13:12:18 +0300

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 mod shell;
 mod cmus;
 
-fn main() {
-    let cmus = cmus::CmusQuery::new();
+use notify_rust::Notification;
+use clap::Parser; 
+use std::{thread, time::Duration};
 
-    if !cmus.remote.is_empty() {
-        println!("{} status: {}", cmus.status_symbol.unwrap(), cmus.status.unwrap());
-        println!("file: {}", cmus.file.unwrap());
-        println!("duration: {}", cmus.duration.unwrap());
-        println!("position: {}", cmus.position.unwrap());
-        println!("time left: -{}", cmus.time_left.unwrap());
-        println!("repeat: {}", cmus.repeat.unwrap());
+/// Cmus rpc
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Don't send notifications when playing new songs
+    #[clap(short = 'n', long, value_parser)]
+    pub no_notification: bool,
+
+    #[clap(short, long, value_parser)]
+    pub debug: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut current_song = String::new();
+
+    loop {
+        let cmus = cmus::CmusQuery::new();
+
+        if cmus.remote.is_empty() {
+            break;
+        }
+
+        if cmus.title != current_song {
+            // send notifications here!
+            if args.no_notification {
+                continue;
+            }
+
+            Notification::new()
+                .summary("Now playing!")
+                .body(&format!("{} - {}", cmus.title, cmus.artist))
+                .urgency(notify_rust::Urgency::Low)
+                .show()
+                .expect("Failed to send notification");
+        }
+
+        current_song = cmus.title.to_string();
+
+        println!("{} - {}", cmus.title, cmus.artist);
+
+        thread::sleep(Duration::from_secs(1));
     }
-    
 }
