6d6989f5f6048fae9c2b45603097bbb0899d983b
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-18 20:48:30 +0300

    check if cmus-rpc is already running or not

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 mod app;
 mod args;
 
+use std::process::exit;
+
 use app::app;
 use args::Args;
 use clap::Parser;
 use discord_rpc_client::Client;
 
 fn main() {
+    if already_running() {
+        println!("cmus-rpc is already running!");
+        exit(1);
+    }
+
     let args = Args::parse();
     let mut rpc = Client::new(args.client_id);
 
@@ -17,3 +24,14 @@ fn main() {
     app(args, rpc);
 }
 
+/// check if cmus-rpc is already running
+fn already_running() -> bool {
+    let p = std::process::Command::new("pgrep").arg("--count").arg("cmus-rpc").output().expect("Failed to run pgrep");
+    let mut p_string = String::from_utf8(p.stdout).unwrap();
+
+    p_string.pop();
+
+    let processes_count: u32 = p_string.parse().unwrap();
+
+    processes_count >= 2
+}
