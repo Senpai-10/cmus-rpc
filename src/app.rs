82f78d755d4fd1bd81408489b107f3f2e24545f7
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-18 06:58:37 +0300

    comment old code

--- src/app.rs
+++ src/app.rs
@@ -1,55 +1,55 @@
-use crate::args::Args;
-use crate::parser::CmusQuery;
-use discord_rpc_client::Client;
-use notify_rust::Notification;
-use std::{thread, time::Duration};
+// use crate::args::Args;
+// use crate::parser::CmusQuery;
+// use discord_rpc_client::Client;
+// use notify_rust::Notification;
+// use std::{thread, time::Duration};
 
-pub fn app(args: Args, mut rpc: Client) -> () {
-    let mut current_song = String::new();
+// pub fn app(args: Args, mut rpc: Client) -> () {
+//     let mut current_song = String::new();
 
-    loop {
-        let cmus = CmusQuery::new();
+//     loop {
+//         let cmus = CmusQuery::new();
 
-        if cmus.remote.is_empty() {
-            println!("cmus is not running!");
-            if !args.debug {
-                rpc.clear_activity().expect("Failed to clear activity");
-            }
-            thread::sleep(Duration::from_secs(3));
-            continue;
-        }
+//         if cmus.remote.is_empty() {
+//             println!("cmus is not running!");
+//             if !args.debug {
+//                 rpc.clear_activity().expect("Failed to clear activity");
+//             }
+//             thread::sleep(Duration::from_secs(3));
+//             continue;
+//         }
 
-        if cmus.status == "playing" {
-            if cmus.title != current_song {
-                if !args.no_notifications {
-                    Notification::new()
-                        .summary("Now playing!")
-                        .body(&format!("{} - {}", cmus.title, cmus.artist))
-                        .urgency(notify_rust::Urgency::Low)
-                        .show()
-                        .expect("Failed to send notification");
-                }
-            }
+//         if cmus.status == "playing" {
+//             if cmus.title != current_song {
+//                 if !args.no_notifications {
+//                     Notification::new()
+//                         .summary("Now playing!")
+//                         .body(&format!("{} - {}", cmus.title, cmus.artist))
+//                         .urgency(notify_rust::Urgency::Low)
+//                         .show()
+//                         .expect("Failed to send notification");
+//                 }
+//             }
 
-            current_song = cmus.title.to_string();
+//             current_song = cmus.title.to_string();
 
-            println!("{} - {} (-{})", cmus.title, cmus.artist, cmus.time_left);
+//             println!("{} - {} (-{})", cmus.title, cmus.artist, cmus.time_left);
 
-            if !args.debug {
-                rpc.set_activity(|activity| {
-                    activity
-                        .details(format!("{}", cmus.title))
-                        .state(format!("{} (-{})", cmus.artist, cmus.time_left))
-                        .assets(|asset| asset.large_image(args.client_large_image.as_str()))
-                })
-                .expect("Failed to set activity");
-            }
-        } else {
-            if !args.debug {
-                rpc.clear_activity().expect("Failed to clear activity");
-            }
-        }
+//             if !args.debug {
+//                 rpc.set_activity(|activity| {
+//                     activity
+//                         .details(format!("{}", cmus.title))
+//                         .state(format!("{} (-{})", cmus.artist, cmus.time_left))
+//                         .assets(|asset| asset.large_image(args.client_large_image.as_str()))
+//                 })
+//                 .expect("Failed to set activity");
+//             }
+//         } else {
+//             if !args.debug {
+//                 rpc.clear_activity().expect("Failed to clear activity");
+//             }
+//         }
 
-        thread::sleep(Duration::from_millis(args.interval));
-    }
-}
+//         thread::sleep(Duration::from_millis(args.interval));
+//     }
+// }
