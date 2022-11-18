99ab6ff97e2adf60a4568332ae6c9948ea8da420
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-19 02:22:42 +0300

    don't use hardcoded string for prog name

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@ mod app;
 mod args;
 
 use std::process::exit;
-
 use app::app;
 use args::Args;
 use clap::Parser;
 use discord_rpc_client::Client;
+use std::env;
+use std::ffi::OsStr;
+use std::path::Path;
 
 fn main() {
-    if already_running() {
-        println!("cmus-rpc is already running!");
+    let prog_name = match get_prog_name() {
+        Some(v) => v,
+        None=> return
+    };
+
+    if already_running(&prog_name) {
+        println!("'{}' is already running!", prog_name);
         exit(1);
     }
 
@@ -25,8 +32,13 @@ fn main() {
 }
 
 /// check if cmus-rpc is already running
-fn already_running() -> bool {
-    let p = std::process::Command::new("pgrep").arg("--count").arg("cmus-rpc").output().expect("Failed to run pgrep");
+fn already_running(prog_name: &String) -> bool {
+    let p = std::process::Command::new("pgrep")
+        .arg("--count")
+        .arg(prog_name)
+        .output()
+        .expect("Failed to run pgrep");
+
     let mut p_string = String::from_utf8(p.stdout).unwrap();
 
     p_string.pop();
@@ -34,4 +46,14 @@ fn already_running() -> bool {
     let processes_count: u32 = p_string.parse().unwrap();
 
     processes_count >= 2
+}
+
+fn get_prog_name() -> Option<String> {
+    env::current_exe()
+        .ok()
+        .as_ref()
+        .map(Path::new)
+        .and_then(Path::file_name)
+        .and_then(OsStr::to_str)
+        .map(String::from)
 }
