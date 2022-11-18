d762235f4ad701bd5ac57893af2a921618a071ad
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-18 06:57:33 +0300

    remove target dir

--- cmus-wrapper/target/debug/deps/cmus_wrapper-9ac374156ed45c39.d
+++
@@ -1,7 +0,0 @@
-/home/senpai/projects/rust/cmus-rpc/cmus-wrapper/target/debug/deps/cmus_wrapper-9ac374156ed45c39.rmeta: src/lib.rs src/shell.rs src/status/mod.rs
-
-/home/senpai/projects/rust/cmus-rpc/cmus-wrapper/target/debug/deps/cmus_wrapper-9ac374156ed45c39.d: src/lib.rs src/shell.rs src/status/mod.rs
-
-src/lib.rs:
-src/shell.rs:
-src/status/mod.rs:
